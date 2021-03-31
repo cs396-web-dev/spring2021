ab707293e1144e46a14167231fedbb5f6bf0a208
Sarah <vanwars@gmail.com>
2021-03-31 12:01:51 -0500

    Posting L2

--- course-files/lectures/lecture02/answers/more_practice_2.js
+++ course-files/lectures/lecture02/answers/more_practice_2.js
@@ -1,6 +1,4 @@
-console.log("Hello, World!");
-listPrint(["line1", "line2", "line3"]);
-fibonacci(12);
+
 
 /* Edit the below functions: */
 
@@ -18,5 +16,23 @@ function fibonacci(n) {
     for (let i = 0; i < n - 2; i++) {
         nums.push(nums[i] + nums[i + 1]);
     }
-    console.log(nums)
+    return nums
 }
+
+
+/* below you will find the function calls and expected output: */
+
+listPrint(["line1", "line2", "line3"]);
+/*
+Expected output:
+line1
+line2
+line3
+*/
+
+console.log(fibonacci(9));
+
+/*
+Expected output:
+[ 0,  1,  1,  2,  3,  5,  8, 13, 21 ]
+*/
