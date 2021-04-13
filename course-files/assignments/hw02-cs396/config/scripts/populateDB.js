d29457f1e1fe10171d870bb207227f4a7079e19e
Sarah <vanwars@gmail.com>
2021-04-12 21:58:17 -0500

    DB populate script has been simplified

--- course-files/assignments/hw02-cs396/config/scripts/populateDB.js
+++ course-files/assignments/hw02-cs396/config/scripts/populateDB.js
@@ -11,10 +11,8 @@ const configObj = require("../config");
 const config = configObj[env || "development"];
 const mongoose = require("mongoose");
 
-const clone = item => JSON.parse(JSON.stringify(item));
 
 const populate = (callback) => {
-    // console.log(config.database);
     // console.log("Trying to connect to database...");
     mongoose.connect(config.database, config.mongoConfig, err => {
         if (err) {
@@ -28,38 +26,10 @@ const populate = (callback) => {
                 schemas.map(schema => schema.deleteMany())
             )
             .then(() => {
-                // console.log("Populating database...");
-                // then create all of the doctors:
-                return Promise.all(
-                    // Each of these database commits is 
-                    // issued asynchronously. The Promise.all
-                    // waits 'til all have completed before moving on...
-                    data.doctors.map(obj => {
-                        obj.doc_id = obj._id; // important to map the relationships between doc and companion
-                        return Doctor.create(obj).save()
-                    })
-                );
+                return Doctor.insertMany(data.doctors);
             })
-            .then(doctors => {
-                // create a lookup table of previous doc ids -> auto-generated ids:
-                const docIdLookup = {};
-                doctors.forEach(doc => {
-                    docIdLookup[doc.doc_id] = doc;
-                });
-                return docIdLookup;
-            })
-            .then((docIdLookup) => {
-                return Promise.all(
-                    // then create all of the companions:
-                    data.companions.map(obj => {
-                        const item = clone(obj)
-                        const docIds = item.doctors.map(id => {
-                            return '' + docIdLookup[id]._id
-                        });
-                        item.doctors = docIds;
-                        return Companion.create(item).save();
-                    })
-                );
+            .then(() => {
+                return Companion.insertMany(data.companions);
             })
             .catch(err => {
                 console.log(err);
@@ -69,7 +39,6 @@ const populate = (callback) => {
                 // console.log("Database populated successfully.");
                 if (callback) {
                     callback();
-                    // process.exit(0);
                 } else {
                     console.log('Exiting');
                     process.exit(0);
